d1f544c15cbef03b6cc818edf52f811c0f1eb67a
shshafin999 <113065728+shshafin999@users.noreply.github.com>
2025-02-02 19:00:53 +0600

    Create class3.cpp

---
+++ class3.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {                    
+
+    // Intro to C++.
+    // Using cin, cout not scanf, printf.
+    // To print new line : '\n' or endl;
+    // cout << a << '\n';
+    // cout << a << endl;
+
+
+    // floor(n), ceil(n), log2(n), log10(n), sqrt(n), sqrtl(n), pow(base, power), __gcd(a, b). 
+    // lcm = (a * b) / __gcd(a, b).
+    // pi = acos(-1).
+    // set precision : 
+    //                cout << fixed << setprecision(10) << n << endl;
+
+
+    // homework
+    // 1. Find log7(100).
+    // 2. Find x from ax^2 + bx + c = 0.
+    // 3. Find degree value from radians.
+
+    return 0;
+}
