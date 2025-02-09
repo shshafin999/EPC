54de3bd448fe58c86863546750d1e40f4373c445
shshafin999 <113065728+shshafin999@users.noreply.github.com>
2025-02-09 20:33:30 +0600

    Update class4.cpp

--- class4.cpp
+++ class4.cpp
@@ -20,68 +20,73 @@ int main() {
 
     
     
-    // Print Numbers from 1 to 100.
+    //1. Print Numbers from 1 to 100.
 
-    // Print Numbers from 100 to 1.
+    //2. Print Numbers from 100 to 1.
     
-    // Find the sum of the numbers from 1 to 100.
+    //3. Find the sum of the numbers from 1 to 100.
 
-    // Find the sum of the cubes of numbers from 1 to 100.
+    //4. Find the sum of the cubes of numbers from 1 to 100.
 
-    // Find the sum of the numbers from 1 to 100 that are not divisible by 5.
+    //5. Find the sum of the numbers from 1 to 100 that are not divisible by 5.
 
-    // Find the sum of the numbers from 1 to 100 that are not divisible by either 5 or 7.
+    //6. Find the sum of the numbers from 1 to 100 that are not divisible by either 5 or 7.
 
-    // Find the sum of the numbers from 1 to 100 that are not divisible by 5 and 7.
+    //7. Find the sum of the numbers from 1 to 100 that are not divisible by 5 and 7.
 
-    // Print the Collatz sequence for a given number n:
-    //     if n is even -> n = n / 2
-    //     if n is odd -> n = 3 * n + 1
-    // Repeat this until n = 1.
-    // input format : n = 3
-    // output format : 3 10 5 16 8 4 2 1
+    //8. Find the sum of the following sequence.
+    //   1 + 1/2 + 1/3 + .... + 1/n
+    //   intput : given a number n
+    //   output : sum
 
-    // Print Fibonacci series upto 100.
+    //9. Print the Collatz sequence for a given number n:
+    //      if n is even -> n = n / 2
+    //      if n is odd -> n = 3 * n + 1
+    //  Repeat this until n = 1.
+    //  input format : n = 3
+    //  output format : 3 10 5 16 8 4 2 1
 
-    // Print this Right Triangle
-    // *
-    // * *
-    // * * *
-    // * * * * 
-    // * * * * * 
-    // * * * * * * 
-    // * * * * * * *
+    //10. Print Fibonacci series upto 100.
+
+    //11. Print this Right Triangle
+    //  *
+    //  * *
+    //  * * *
+    //  * * * * 
+    //  * * * * * 
+    //  * * * * * * 
+    //  * * * * * * *
 
     
-    // Calculate BMI for 5 persons . . . Using LOOP
-    // input format : height weight
-    // 1.5 50
-    // 1.6 51
-    // 1.7 55
-    // 1.2 70
-    // 1.3 60
-    // output format : 
-    // 22.2222
-    // 19.9219
-    // 19.0311
-    // 48.6111
-    // 35.503
+    //12. Calculate BMI for 5 persons . . . Using LOOP
+    //  input format : height weight
+    //  1.5 50
+    //  1.6 51
+    //  1.7 55
+    //  1.2 70
+    //  1.3 60
+    //  output format : 
+    //  22.2222
+    //  19.9219
+    //  19.0311
+    //  48.6111
+    //  35.503
 
 
 
 
 
-    // Print the multiplication table of 5 from 1 to 10. Using LOOP
-    // output format
-    // 5 x 1 = 5
-    // 5 x 2 = 10
-    // 5 x 3 = 15
-    // 5 x 4 = 20
-    // .
-    // . 
-    // . 
-    // 5 x 9 = 45 
-    // 5 x 10 = 50 
+    //13. Print the multiplication table of 5 from 1 to 10. Using LOOP
+    //  output format
+    //  5 x 1 = 5
+    //  5 x 2 = 10
+    //  5 x 3 = 15
+    //  5 x 4 = 20
+    //  .
+    //  . 
+    //  . 
+    //  5 x 9 = 45 
+    //  5 x 10 = 50 
  
     return 0;
 }
