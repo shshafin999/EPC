5e6bb7447f20247c6b6558bf6997c5d9f896683a
shshafin999 <113065728+shshafin999@users.noreply.github.com>
2025-03-19 06:23:00 +0600

    Create last_class.cpp

---
+++ last_class.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // array
+    // intro to problem solving and competitive programming
+    // exploring online judges
+
+    return 0;
+}
